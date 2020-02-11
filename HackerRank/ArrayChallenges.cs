35f9c63fa6f88da7da1fac17667dfdc48e4f08c5
James <james@Mr-Gorilla.local>
2020-02-11 22:06:03 +0900

    updated the menus

---
+++ HackerRank/ArrayChallenges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace HackerRank
+{
+    public class ArrayChallenges
+    {
+
+        //Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+        public static void SockMerchant(string str)
+        {
+            int oneNum;
+            List<int> nums = new List<int>();
+            string[] tokens = str.Split(",");
+
+            foreach (string s in tokens)
+            {
+                    oneNum =  Int32.Parse(s);
+                    nums.Add(oneNum);
+            }
+
+            int[] numArr = nums.ToArray();
+
+           
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -6,8 +6,58 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("- - - - - - - - - - HACKER RANK CHALLENGES - - - - - - - - - - - ");
-            Console.WriteLine("Please select an option:");
+            MainMenu();
         }
+
+        static void MainMenu()
+        {
+            int choice;
+
+            Console.WriteLine("- - - - - - - - - - HACKER RANK CHALLENGES - - - - - - - - - - - ");
+            Console.WriteLine("");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("1. Array Manipulation");
+            Console.WriteLine("2. String Manipulation");
+            Console.WriteLine("3. Sorting");
+            choice = int.Parse(Console.ReadLine());
+
+
+            switch (choice)
+            {
+                case 1:
+                    Console.Clear();
+                    ArrayManipulation();
+                    break;
+
+            }
+        }
+
+
+        static void ArrayManipulation()
+        {
+            Console.WriteLine("- - - - - - - - - - ARRAY MANIPULATION - - - - - - - - - - - ");
+            Console.WriteLine("");
+            Console.WriteLine("Please select an option:    (b --> back)");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("1. Sock Merchant");
+            string choice = Console.ReadLine();
+
+
+            switch (choice)
+            {
+                case "1":
+                    Console.WriteLine("Please enter numbers seperated by commas");
+                    string word = Console.ReadLine();
+                    ArrayChallenges.SockMerchant(word);
+                    break;
+                case "b":
+                    Console.Clear();
+                    MainMenu();
+                    break;
+
+            }
+        }
+
     }
 }
